9bffb9b2f57bc18a7410477824635bae10f0567c
Artkiller66 <webworking66@gmail.com>
2019-05-16 23:54:28 +0400

    step 07

--- Jscript.js
+++ Jscript.js
@@ -1,21 +1,22 @@
 window.onload = function() {
 
-var modal = document.getElementById('myModal');
-var vid1 = document.getElementById('vid');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+    let modal = document.getElementById('myModal');
+    let vid1 = document.getElementById('vid');
+    let btn = document.getElementById("myBtn");
+    let span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-};
+    btn.onclick = function() {
+        modal.style.display = "block";
+        vid1.play();
+    };
 
-span.onclick = function() {
-    modal.style.display = "none";
-    vid1.pause();
-    vid1.currentTime = 0;
-};
+    span.onclick = function() {
+        modal.style.display = "none";
+        vid1.pause();
+        vid1.currentTime = 0;
+    };
 
-window.onclick = function(event) {
+    window.onclick = function(event) {
         if (event.target === modal) {
             modal.style.display = "none";
             vid1.pause();
